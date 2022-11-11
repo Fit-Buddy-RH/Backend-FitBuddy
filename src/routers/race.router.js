5b279e4aa94fc741f3193fc9cb331e55b35fca6c
Héctor Barboza <78834438+hector-bapz@users.noreply.github.com>
2022-11-11 13:52:17 -0700

    update race.router.js

--- src/routers/race.router.js
+++ src/routers/race.router.js
@@ -1,11 +1,33 @@
 import express from "express";
+import {auth} from "../middleware/auth.js"
 import * as raceUseCase from "../useCase/race.use.js";
+import * as UserUseCase from "../useCase/user.use.js"
+import * as raceRequestUseCase from "../useCase/raceRequest.use.js"
 import * as commentUseCase from "../useCase/comment.use.js";
 
 const router = express.Router();
 router.use(express.json());
 
-router.get("./", async (request, response, next) => {
+router.post("/", auth, async (request, response, next) => {
+  try {
+    const { body: newDataRace, auth } = request;
+    const newRace = await raceUseCase.create(newDataRace, auth);
+    
+    await UserUseCase.addRace(newRace.id)
+    
+    response.json({
+      success: true,
+      data: {
+        race: newRace,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+router.get("/all", async (request, response, next) => {
   try {
     const allRaces = await raceUseCase.getAll();
     response.json({
@@ -19,10 +41,10 @@ router.get("./", async (request, response, next) => {
   }
 });
 
-router.get("/:idRace", async (request, response, next) => {
+router.get("/me", auth, async (request, response, next) => {
   try {
-    const id = request.params.idRace;
-    const race = await raceUseCase.getById(id);
+    const {auth: id} = request
+    const race = await raceUseCase.getByUser(id);
     response.json({
       success: true,
       data: {
@@ -37,6 +59,66 @@ router.get("/:idRace", async (request, response, next) => {
 router.get("/user/:idUser", async (request, response, next) => {
   try {
     const id = request.params.idUser;
+    const race = await raceUseCase.getByUser(id);
+    response.json({
+      success: true,
+      data: {
+        race: race,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/assistants/me", auth, async (request, response, next) => {
+  try {
+    const {auth: id} = request
+    const race = await raceUseCase.getByAssistant(id);
+    response.json({
+      success: true,
+      data: {
+        race: race,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/assistants/user/:idUser", async (request, response, next) => {
+  try {
+    const id = request.params.idUser;
+    const race = await raceUseCase.getByAssistant(id);
+    response.json({
+      success: true,
+      data: {
+        race: race,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/dashboard", auth, async (request, response, next) => {
+  try {
+    const {location, auth:id} = request
+    const race = await raceUseCase.getByZipcode(location, id);
+    response.json({
+      success: true,
+      data: {
+        race: race,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:idRace", async (request, response, next) => {
+  try {
+    const {idRace} = request.params
     const race = await raceUseCase.getById(id);
     response.json({
       success: true,
@@ -49,11 +131,13 @@ router.get("/user/:idUser", async (request, response, next) => {
   }
 });
 
+
 router.patch("/:idRace", auth, async (request, response, next) => {
   try {
-    const id = request.params.idRace;
+    const {idRace} = request.params
     const newDataRace = request.body;
-    const updatedRace = await raceUseCase.update(id, newDataRace);
+    const {auth:idUser} = request
+    const updatedRace = await raceUseCase.update(idRace, idUser, newDataRace);
 
     response.json({
       success: true,
@@ -68,10 +152,12 @@ router.patch("/:idRace", auth, async (request, response, next) => {
 
 router.delete("/:idRace", auth, async (request, response, next) => {
   try {
-    const id = request.params.idRace;
-    const race = await raceUseCase.deleteById(id);
-    const commentsRemoval = await commentUseCase.deleteRaceComments(id);
-
+    const {idRace} = request.params
+    const {auth:idUser} = request
+    const race = await raceUseCase.deleteById(idRace, idUser);
+    await UserUseCase.deleteRace()
+    await commentUseCase.deleteByRace(idRace);
+    await raceRequestUseCase.deleteByRace()
     response.json({
       success: true,
       race: race,
@@ -82,20 +168,5 @@ router.delete("/:idRace", auth, async (request, response, next) => {
   }
 });
 
-router.post("/", async (request, response, next) => {
-  try {
-    const { body: newDataRace } = request;
-    const newRace = await raceUseCase.create(newDataRace);
-
-    response.json({
-      success: true,
-      data: {
-        race: newRace,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
 
 export default router;
